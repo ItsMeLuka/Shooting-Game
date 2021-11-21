cec65d08505a743b92d17fa4e47f59c53c393601
ItsMeLuka <Grubkiller00289@gmail.com>
2021-11-21 16:29:21 +0100

    Player sprite animation

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame as pg
-
+import os
 
 pg.init()
 
-screen_width = 800
-screen_height = int(screen_width * 0.8)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
-screen = pg.display.set_mode((screen_width, screen_height))
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption('Shooter')
 
 #set framerate
 clock = pg.time.Clock()
-fps = 60
+FPS = 60
 
+#define game variables
+GRAVITY = 0.75
 
 #define player action variables
 moving_left = False
@@ -20,24 +22,46 @@ moving_right = False
 
 #define colors
 BG = (144, 201, 120)
+RED = (255, 0, 0)
 
 def draw_bg():
     screen.fill(BG)
+    pg.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 
 class Soldier(pg.sprite.Sprite):
     def __init__(self,  char_type,  x, y, scale, speed):
         pg.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
         self.flip = False
-        img = pg.image.load(f'img/{self.char_type}/Idle/0.png')
-        self.image = pg.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pg.time.get_ticks()
+
+        #load all images for players
+        animations_types = ['Idle', 'Run', 'Jump']
+        for animation in animations_types:
+            #reset temporary list of images
+            temp_list = []
+            #count number of files in folder
+            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pg.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+                img = pg.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
-    def update(self, moving_left, moving_right):
+    def move(self, moving_left, moving_right):
         #reset movement variables
         dx = 0
         dy = 0
@@ -51,9 +75,47 @@ class Soldier(pg.sprite.Sprite):
             self.flip = False
             self.direction = 1
 
+        #jump
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        #check collision with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+            
         #update rectangle position
         self.rect.x += dx
         self.rect.y += dy
+
+    def update_animation(self):
+        #update animation
+        ANIMATION_COOLDOWN = 100
+        #update image depending on current frame
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pg.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pg.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        #check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #update the animation settings
+            self.frame_index = 0
+            self.update_time = pg.time.get_ticks()
 
     def draw(self):
         screen.blit(pg.transform.flip(self.image, self.flip, False), self.rect)
@@ -65,12 +127,22 @@ enemy = Soldier('enemy', 400, 200, 3, 5)
 run = True
 while run:
 
-    clock.tick(fps)
+    clock.tick(FPS)
     draw_bg()
 
+    player.update_animation()
     player.draw()
+    enemy.update_animation()
     enemy.draw()
-    player.update(moving_left, moving_right)
+    #update player actions
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)#2: jump
+        elif moving_left or moving_right:
+            player.update_action(1)#1: run
+        else:
+            player.update_action(0)#0: idle
+    player.move(moving_left, moving_right)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -80,6 +152,8 @@ while run:
                 moving_left = True
             if event.key == pg.K_d:
                 moving_right = True
+            if event.key == pg.K_w and player.alive:
+                player.jump = True
             if event.key == pg.K_ESCAPE:
                 run = False
 
@@ -90,6 +164,7 @@ while run:
                 moving_left = False
             if event.key == pg.K_d:
                 moving_right = False
+            
 
     
     pg.display.update()
